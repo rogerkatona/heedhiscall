868417e00658b8ac92e8cdd19b0e21fc624236aa
Roger Katona <roger.katona@gmail.com>
2022-06-27 16:03:54 -0400

    Set event planner assets to open in a new page.

--- components/planner-resources.js
+++ components/planner-resources.js
@@ -24,11 +24,14 @@ export default function PlannerResources() {
                                 <div className={` lg:w-4/5 w-full pt-6 ${plannerItems.id % 2 !== 0  ? 'lg:pl-6' : 'lg:pr-6'}`}>
                                     <div className='text-2xl text-gray.700 '>{plannerItems.title}</div>
                                     <p className="pb-6">{plannerItems.description}</p>
-                                    <Link href={plannerItems.buttonHref}>
+                                    <a
+                                        href={plannerItems.buttonHref}
+                                        target="_blank"
+                                        rel="noreferrer">
                                         <button className="hover:bg-rust.800 hover:text-white.100 text-xs text-rust.700 uppercase px-4 py-3 border border-rust.500 rounded-lg">
                                             {plannerItems.buttonText}
                                         </button>
-                                    </Link>
+                                    </a>
                                 </div>
                             </div>
                         </section>
