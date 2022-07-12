7cb814336ffbcbf10360623c968c0d7230f0840e
Roger Katona <roger.katona@gmail.com>
2022-07-12 08:57:21 -0400

    Minor text update

--- components/bookACall_01.js
+++ components/bookACall_01.js
@@ -24,7 +24,7 @@ export default function BookACall_01() {
                             The cost of turnover in the medical field and decreased productivity from burnout is estimated at $4.6 B annually. Burnout can be a larger problem for female physicians and medical professionals due to &quot;hidden&quot; stressors of gender discrimination in the workplace and family expectations in the home.
                         </div>
                         <div className="text-gray.700 pt-4">
-                            Hospitals and event planners can use their diversity inclusion budget to help their female physicians, mid-level practitioners and nurses reclaim joy in their career which translates to better patient care, less turnover, and a reduction in medical errors.
+                            Hospitals and event planners can use their diversity inclusion budget to help their female physicians, mid-level practitioners and nurses reclaim joy in their career which translates to better patient care, less turnover, and a reduction in medical errors. Let's work together to help retain our women in medicine and reduce the negative impact on the healthcare industry!
                         </div>
                         <div className="text-gray.700 pt-4">
                             Book a call now to see how Dr. Sundar can help improve engagement within your hospital!
