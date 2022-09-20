2d8fee46d33356cd78f174a218692739f0299faf
Roger Katona <roger.katona@gmail.com>
2022-09-20 08:59:47 -0700

    Updated missionStatement.js.  Simple text change

--- components/missionStatement.js
+++ components/missionStatement.js
@@ -5,10 +5,10 @@ export default function MissionStatement() {
         <section className="max-w-7xl mx-auto mx-12">
             <div className="flex flex-col justify-center xl:px-0 px-12 py-12">
                 <div className="font-bebasNeue md:text-4xl text-3xl text-gray.700">
-                    Professional speaker . Motivator . Certified Life Coach
+                    Transformational speaker . Master Certified Professional Coach
                 </div>
                 <div className="pt-4">
-                    Dr. Kalpana Sundar is a motivational speaker who focuses on guiding women in medicine toward fulfilling careers and personal lives. She helps women navigate the medical field with clarity to work towards their version of success.  Dr Sundar’s workshops and breakout sessions inspire women to regain career fulfillment.
+                    Dr. Kalpana Sundar is a transformational speaker who focuses on guiding women in medicine toward fulfilling careers and personal lives. She helps women navigate the medical field with clarity to work towards their version of success.  Dr Sundar’s workshops and breakout sessions inspire women to regain career fulfillment.
                 </div>
             </div>
         </section>
